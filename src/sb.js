a2ffb65c809f4b1d1c9368611a0230d74418bd1c
Denis Chinin <chinin@immo.ru>
2014-01-31 15:09:27 +0400

    add SB.ready calling when smartbox inited

--- src/sb.js
+++ src/sb.js
@@ -3,112 +3,119 @@
  */
 (function ( window, undefined ) {
 
-	// save in case of overwrite
-	var document = window.document,
-		_inited = false,
-		readyCallbacks = [];
+  // save in case of overwrite
+  var document = window.document,
+    _inited = false,
+    _ready = false,
+    readyCallbacks = [];
 
-	var SB = {
+  var SB = {
 
-		config: {
-			/**
-			 * Платформа, которая будет использоваться в случае, когда detectPlatform вернул false
-			 * ex: browser, samsung, lg
-			 * @type: {String}
-			 */
-			defaultPlatform: 'browser',
+    config: {
+      /**
+       * Платформа, которая будет использоваться в случае, когда detectPlatform вернул false
+       * ex: browser, samsung, lg
+       * @type: {String}
+       */
+      defaultPlatform: 'browser',
 
-			/**
-			 * Платформа, используемая по умолчанию, метод detectPlatform не вызывается
-			 *  ex: browser, samsung, lg
-			 * @type: {String}
-			 */
-			currentPlatform: ''
-		},
-
-		isInited: function () {
-			return _inited;
-		},
-
-		/**
-		 * Main function
-		 * @param cb {Function} callback after initialization
-		 */
-		ready: function ( cb ) {
-			readyCallbacks.push(cb);
-		},
-
-    readyForPlatform: function(platform, cb){
-        var self=this;
-        this.ready(function(){
-            if(platform==self.currentPlatform.name){
-                cb();
-            }
-        });
+      /**
+       * Платформа, используемая по умолчанию, метод detectPlatform не вызывается
+       *  ex: browser, samsung, lg
+       * @type: {String}
+       */
+      currentPlatform: ''
     },
 
-		/**
-		 * Applying all ready callbacks
-		 * @private
-		 */
-		_onReady: function () {
-			for ( var i = 0, len = readyCallbacks.length; i < len; i++ ) {
-				readyCallbacks[i].call(this);
-			}
-		},
+    isInited: function () {
+      return _inited;
+    },
 
-		initialise: function () {
-			var self = this,
-				utils = this.utils;
+    /**
+     * Main function
+     * @param cb {Function} callback after initialization
+     */
+    ready: function ( cb ) {
+      if ( _ready ) {
+        cb();
+      } else {
+        readyCallbacks.push(cb);
+      }
+    },
 
-			if ( _inited ) {
-				return;
-			}
+    readyForPlatform: function ( platform, cb ) {
+      var self = this;
+      this.ready(function () {
+        if ( platform == self.currentPlatform.name ) {
+          cb();
+        }
+      });
+    },
 
-			window.$$log = utils.log.log;
-			window.$$error = utils.error;
+    /**
+     * Applying all ready callbacks
+     * @private
+     */
+    _onReady: function () {
+      _ready = true;
 
-			//$$log('!!!!!!!!!LOG: initialising SB');
+      for ( var i = 0, len = readyCallbacks.length; i < len; i++ ) {
+        readyCallbacks[i].call(this);
+      }
+    },
 
-			SB.platforms.initialise(function ( currentPlatform ) {
-				self.currentPlatform = currentPlatform;
-				_inited = true;
+    initialise: function () {
+      var self = this,
+        utils = this.utils;
+
+      if ( _inited ) {
+        return;
+      }
+
+      window.$$log = utils.log.log;
+      window.$$error = utils.error;
+
+      //$$log('!!!!!!!!!LOG: initialising SB');
+
+      SB.platforms.initialise(function ( currentPlatform ) {
+        self.currentPlatform = currentPlatform;
+        _inited = true;
 
         //prevent calling before other dom ready callbacks
-        setTimeout(function (  ) {
+        setTimeout(function () {
           self._onReady();
         });
-			});
-		}
-	};
+      });
+    }
+  };
 
-	SB.utils = {
-		/**
-		 * Show error message
-		 * @param msg
-		 */
-		error: function ( msg ) {
-			$$log(msg, 'error');
-		},
+  SB.utils = {
+    /**
+     * Show error message
+     * @param msg
+     */
+    error: function ( msg ) {
+      $$log(msg, 'error');
+    },
 
-		/**
-		 * Show messages in log
-		 * all functionality in main.js
-		 */
-		log: {
-			log: $.noop,
-			state: $.noop,
-			show: $.noop,
-			hide: $.noop,
-			startProfile: $.noop,
-			stopProfile: $.noop
-		},
+    /**
+     * Show messages in log
+     * all functionality in main.js
+     */
+    log: {
+      log: $.noop,
+      state: $.noop,
+      show: $.noop,
+      hide: $.noop,
+      startProfile: $.noop,
+      stopProfile: $.noop
+    },
 
     legend: {}
-	};
+  };
 
-	$(function () {
-		SB.initialise();
-	});
-	window.SB = SB;
+  $(function () {
+    SB.initialise();
+  });
+  window.SB = SB;
 })(this);
