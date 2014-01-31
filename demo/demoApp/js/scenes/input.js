4279fe03dac4ef27c51452a3a27f2a066bca1c2f
Denis Chinin <chinin@immo.ru>
2014-01-31 16:10:15 +0400

    add demoApp

---
+++ demo/demoApp/js/scenes/input.js
@@ -0,0 +1,48 @@
+(function () {
+  "use strict";
+  var _inited;
+
+  window.App.scenes.input = {
+    init: function () {
+      this.$el = $('.js-scene-input');
+
+      var $valueText = this.$el.find('.js-input-1-val');
+
+      this.$el.find('.js-input-1')
+        .on('change',function () {
+          $valueText.html(this.value);
+        })
+        .SBInput({
+          keyboard: {
+            type: 'fulltext_ru_nums'
+          }
+        });
+
+      this.$el.find('.js-input-2').SBInput({
+        keyboard: {
+          type: 'email'
+        }
+      });
+
+      this.$el.find('.js-input-3').SBInput({
+        keyboard: {
+          type: 'num'
+        },
+        max: 4
+      });
+
+
+      _inited = true;
+    },
+    show: function () {
+      if (!_inited) {
+        this.init();
+      }
+
+      this.$el.show();
+    },
+    hide: function () {
+      this.$el.hide();
+    }
+  }
+})();
