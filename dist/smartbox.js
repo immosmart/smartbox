3cb726259831c6048de62ddfbfdf68deb5330c9c
Denis Chinin <chinin@immo.ru>
2014-02-28 13:38:07 +0400

    add mag DUID # MAC functions

--- dist/smartbox.js
+++ dist/smartbox.js
@@ -3140,18 +3140,7 @@ SB.createPlatform('browser', {
             this.DUID = 'FIREFOXISBEST';
         }
         return this.DUID;
-    },
-
-    volumeUp: function () {
-    },
-
-    volumeDown: function () {
-    },
-
-    getVolume: function () {
     }
-
-
 });
 
 (function ($) {
@@ -3479,120 +3468,117 @@ SB.readyForPlatform('mag', function () {
 
 (function () {
 
-    var stb;
-    /**
-     * Mag set top box platform description
-     */
-    SB.createPlatform('mag', {
-        keys: {
-            RIGHT: 39,
-            LEFT: 37,
-            DOWN: 40,
-            UP: 38,
-            RETURN: 8,
-            EXIT: 27,
-            TOOLS: 122,
-            FF: 70,
-            RW: 66,
-            NEXT: 34,
-            PREV: 33,
-            ENTER: 13,
-            RED: 112,
-            GREEN: 113,
-            YELLOW: 114,
-            BLUE: 115,
-            CH_UP: 9,
-            CH_DOWN: 9,
-            N0: 48,
-            N1: 49,
-            N2: 50,
-            N3: 51,
-            N4: 52,
-            N5: 53,
-            N6: 54,
-            N7: 55,
-            N8: 56,
-            N9: 57,
-            PRECH: 116,
-            POWER: 85,
-            //SMART: 36,
-            PLAY: 82,
-            STOP: 83,
-            //PAUSE: 99,
-            //SUBT: 76,
-            INFO: 89
-            //REC: 82
-        },
+  var stb;
+  /**
+   * Mag set top box platform description
+   */
+  SB.createPlatform('mag', {
+    keys: {
+      RIGHT: 39,
+      LEFT: 37,
+      DOWN: 40,
+      UP: 38,
+      RETURN: 8,
+      EXIT: 27,
+      TOOLS: 122,
+      FF: 70,
+      RW: 66,
+      NEXT: 34,
+      PREV: 33,
+      ENTER: 13,
+      RED: 112,
+      GREEN: 113,
+      YELLOW: 114,
+      BLUE: 115,
+      CH_UP: 9,
+      CH_DOWN: 9,
+      N0: 48,
+      N1: 49,
+      N2: 50,
+      N3: 51,
+      N4: 52,
+      N5: 53,
+      N6: 54,
+      N7: 55,
+      N8: 56,
+      N9: 57,
+      PRECH: 116,
+      POWER: 85,
+      //SMART: 36,
+      PLAY: 82,
+      STOP: 83,
+      //PAUSE: 99,
+      //SUBT: 76,
+      INFO: 89
+      //REC: 82
+    },
 
-        onDetect: function () {
+    onDetect: function () {
 
-            var isStandBy = false;
+      var isStandBy = false;
 
-            stb = window.gSTB;
+      stb = window.gSTB;
 
-            window.moveTo(0, 0);
-            window.resizeTo(1280, 720);
+      // prohibition of keyboard showing on click keyboard button
+      stb.EnableVKButton(false);
 
-            SB(function () {
-              var $body = $(document.body);
-              $body.on('nav_key:power', function () {
-                var eventName = 'standby_';
-                isStandBy = !isStandBy;
+      window.moveTo(0, 0);
+      window.resizeTo(1280, 720);
 
-                eventName += isStandBy ? 'set' : 'unset';
-                stb.StandBy(isStandBy);
+      SB(function () {
+        var $body = $(document.body);
+        $body.on('nav_key:power', function () {
+          var eventName = 'standby_';
+          isStandBy = !isStandBy;
 
-                // TODO: trigger events on SB
-                $$log('trigger standby event ' + eventName, 'standby');
-                $body.trigger(eventName);
-              });
-            });
+          eventName += isStandBy ? 'set' : 'unset';
+          stb.StandBy(isStandBy);
 
+          // TODO: trigger events on SB
+          $$log('trigger standby event ' + eventName, 'standby');
+          $body.trigger(eventName);
+        });
+      });
 
-            window.localStorage = {
-                setItem: function (name, data) {
-
-                },
-                clear: function () {
-
-                },
-                getItem: function () {
-
-                },
-                removeItem: function () {
-
-                }
-            }
-        },
-
-        detect: function () {
-            return !!window.gSTB;
-        },
-
-        exit: function () {
-            $$log('try to location change');
-            Player.stop(true);
-            gSTB.DeinitPlayer();
-            window.location = 'file:///home/web/services.html';
-        },
-
-        sendReturn: function () {
-            this.exit();
-        },
-
-        getNativeDUID: function () {
+      window.localStorage = {
+        setItem: function ( name, data ) {
 
         },
+        clear: function () {
 
-        volumeUp: function () {
         },
+        getItem: function () {
 
-        volumeDown: function () {
         },
+        removeItem: function () {
 
-        getVolume: function () {
         }
-    });
+      }
+    },
+
+    detect: function () {
+      return !!window.gSTB;
+    },
+
+    exit: function () {
+      $$log('try to location change');
+      Player.stop(true);
+      gSTB.DeinitPlayer();
+      window.location = 'file:///home/web/services.html';
+    },
+
+    sendReturn: function () {
+      this.exit();
+    },
+
+    getMac: function () {
+      return stb.GetDeviceMacAddress();
+    },
+
+    getNativeDUID: function () {
+      return stb.GetDeviceSerialNumber();
+    }
+  });
 
 }());
 
