f34f2e54efec880fd614e24b7a36d7a2819f135c
Denis Chinin <chinin@immo.ru>
2014-03-17 17:27:11 +0400

    setSize methods browser & samsung

--- dist/smartbox.js
+++ dist/smartbox.js
@@ -324,6 +324,35 @@
 
   _.extend(SB, PlatformApi);
 })(this);
+(function () {
+  /**
+   * Plugin constructor
+   * @param name plugin name
+   * @param api plugin public functions
+   * @returns {Plugin}
+   * @constructor
+   */
+  var Plugin = function ( name, api ) {
+    this.name = name;
+
+    _.extend(this, api);
+    return this;
+  };
+
+  Plugin.prototype.config = {};
+  Plugin.prototype.initialize = $.noop;
+  Plugin.prototype.isManuallyInited = false;
+
+  SB.addPlugin = function ( pluginName, pluginApi ) {
+    var plugin;
+    pluginName = '$$' + pluginName;
+    plugin = this.plugins[pluginName] = new Plugin(pluginName, pluginApi);
+
+    if ( !window[pluginName] ) {
+      window[pluginName] = plugin;
+    }
+  }
+})();
 /**
  * Keyboard Plugin
  */
