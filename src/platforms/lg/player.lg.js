2dc1dff3569715f7ef72a7228dde02aff09e7a9f
Artem Poletsky <poletsky@immo.ru>
2014-03-20 11:04:11 +0400

    lg player set size implemented lg widevine drm supported

--- src/platforms/lg/player.lg.js
+++ src/platforms/lg/player.lg.js
@@ -8,18 +8,18 @@ SB.readyForPlatform('lg', function () {
         _init: function () {
 
         },
-        onEvent: function(){
-            if(this.plugin.playState==5){
-                this.state='stop';
+        onEvent: function () {
+            if (this.plugin.playState == 5) {
+                this.state = 'stop';
                 this.trigger('complete');
-            }else if(this.plugin.playState==4){
+            } else if (this.plugin.playState == 4) {
                 this.trigger('error')
             }
         },
         _update: function () {
             var info = this.plugin.mediaPlayInfo();
 
-            if (info && info.duration && !isReady) {
+            if (info && info.width && !isReady) {
                 //$('#log').append('<div>'+info.duration+'</div>');
 
                 //$$log(JSON.stringify(info));
@@ -30,10 +30,10 @@ SB.readyForPlatform('lg', function () {
                 this.videoInfo = {
                     duration: info.duration / 1000
                 };
-                var self=this;
-                if(from){
-                    var self=this;
-                    var onBufEnd=function(){
+                var self = this;
+                if (from) {
+                    var self = this;
+                    var onBufEnd = function () {
                         self.off('bufferingEnd', onBufEnd);
                         self.seek(from);
                     };
@@ -44,11 +44,11 @@ SB.readyForPlatform('lg', function () {
 
             }
 
-            if(!isReady){
+            if (!isReady) {
                 return;
             }
 
-            this.videoInfo.currentTime=info.currentPosition/1000;
+            this.videoInfo.currentTime = info.currentPosition / 1000;
             this.trigger('update');
         },
         onBuffering: function (isStarted) {
@@ -64,11 +64,11 @@ SB.readyForPlatform('lg', function () {
 
             $('#pluginPlayer').remove();
 
-            $('body').append('<object mode3D="'+(options.is3d?'side_by_side':'off')+'" type="video/mp4" data="" width="1280" height="720" id="pluginPlayer" style="z-index: 0; position: absolute; left: 0; top: 0;"></object>');
+            $('body').append('<object mode3D="' + (options.is3d ? 'side_by_side' : 'off') + '" type="video/mp4" data="" width="1280" height="720" id="pluginPlayer" style="z-index: 0; position: absolute; left: 0; top: 0;"></object>');
             this.plugin = $('#pluginPlayer')[0];
             this.$plugin = $(this.plugin);
 
-            var self=this;
+            var self = this;
             this.plugin.onPlayStateChange = function () {
                 self.onEvent.apply(self, arguments);
             }
@@ -76,33 +76,78 @@ SB.readyForPlatform('lg', function () {
                 self.onBuffering.apply(self, arguments);
             }
 
-            this.plugin.onError  = function () {
+            this.plugin.onError = function () {
                 self.trigger('error')
+            }
+
+
+            if (options.drm && options.drm.type == 'widevine') {
+
+
+                self.plugin.setWidevineDrmURL(options.drm.url);
+
+                if (options.drm.streamID)
+                    self.plugin.setWidevineStreamID(options.drm.streamID);
+                if (options.drm.deviceType)
+                    self.plugin.setWidevineDeviceType(options.drm.deviceType);
+                if (options.drm.portalID)
+                    self.plugin.setWidevinePortalID(options.portalID);
+                if (options.drm.storeFront)
+                    self.plugin.setWidevineStoreFront(options.storeFront);
+
+
+                this.$plugin.attr('drm_type', 'videwine');
+
             }
 
 
             this.plugin.data = options.url;
             this.plugin.play(1);
 
-            from= options.from;
+            from = options.from;
         },
-        _pause: function(){
+        _setSize: function (size) {
+
+            //size = this._fixAspectRatio(size);
+
+
+            if (size.width) {
+                this.plugin.width = size.width;
+            }
+
+            if (size.height) {
+                this.plugin.height = size.height;
+            }
+
+            if (size.left) {
+                this.plugin.style.left = size.left + 'px';
+            }
+
+            if (size.top) {
+                this.plugin.style.top = size.top + 'px';
+            }
+        },
+        _pause: function () {
             this.plugin.play(0);
         },
-        _resume: function(){
+        _resume: function () {
             this.plugin.play(1);
         },
         _stop: function () {
             this.plugin.stop();
         },
-        _seek: function(time){
-            this.plugin.seek(time*1000);
+        seek: function (time) {
+            this.plugin.seek(time * 1000);
+        },
+        showOptionMedia: function () {
+            var app = new lge();
+            app.optionMedia();
         },
         audio: {
             set: function (index) {
             },
             get: function () {
-               return [];
+                return [];
             },
             cur: function () {
                 return 0;
