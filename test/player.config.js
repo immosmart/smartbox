46586e97caba8ce8535b054fd503d405baf9678f
Artem Poletsky <poletsky@immo.ru>
2014-03-20 17:10:14 +0400

    ignore realsync, new player test

--- test/player.config.js
+++ test/player.config.js
@@ -1,8 +1,8 @@
 var Config={
-    trailer: 'http://ua-cnt.smart-kino.com//trailers/russkiy_reportazh/korporatsiya_svyatye_motory.mp4',
-    trailerWidth: 640,
-    trailerHeight: 360,
-    trailerDuration: '00:31',
+    trailer: 'https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4',
+    trailerWidth: 426,
+    trailerHeight: 240,
+    trailerDuration: '10:54',
     movie: 'https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4',
     movieAudioTracksLength: 2,
     hls: 'http://phone.pik-tv.com/live/mp4:piktv3pik3tv/playlist.m3u8'
